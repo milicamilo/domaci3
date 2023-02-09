a099846cd76de0d7ec3b818f7ab883a83fbd0321
Milica Milovanovic <milica.milovanovic01@gmail.com>
2023-02-09 10:40:27 +0100

    Izmenjene slike

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { ImPencil } from "react-icons/im";
+import { IoTrashSharp } from "react-icons/io5";
+import { HiPlus, HiTrash } from "react-icons/hi";
 
 function ProductCard({ product }) {
   return (
@@ -15,12 +18,17 @@ function ProductCard({ product }) {
           <strong>{product.cena}</strong>
         </p>
 
-        <button type="button" className="btn btn-custom">
-          Uredi
-        </button>
-        <button type="button" className="btn btn-custom">
-          Obriši
-        </button>
+        <div>
+          <button type="button" className="btn btn-custom tag-pink">
+            <HiPlus />
+          </button>
+          <button type="button" className="btn btn-custom tag-lavanda">
+            <ImPencil />
+          </button>
+          <button type="button" className="btn btn-custom tag-blue">
+            <HiTrash />
+          </button>
+        </div>
       </div>
     </div>
   );
